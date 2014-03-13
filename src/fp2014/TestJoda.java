ddb14d3c1d9088ec5582ce9df3c3a41df1c115e9
torsteig <torsteig@stud.ntnu.no>
2014-03-13 16:17:13 +0100

    joda oda joda

--- src/fp2014/TestJoda.java
+++ src/fp2014/TestJoda.java
@@ -10,14 +10,13 @@ import org.joda.time.LocalTime;
 public class TestJoda {
 	
 	public static void main(String[] args) {
-		LocalDate dato = new LocalDate();
-		LocalDate dato2 = new LocalDate(2014, 3, 12);
-		LocalDate dato3 = new LocalDate(2014, 3, 14);
-		System.out.println(dato);
-
-		System.out.println(dato2.compareTo(dato));
-		System.out.println(dato.compareTo(dato));
-		System.out.println(dato3.compareTo(dato));
+		
+		LocalTime time = new LocalTime();
+		LocalTime time2 = new LocalTime(13,13);
+		
+		System.out.println(time2);
+		
+		System.out.println(time.toString());
 		
 	}
 
